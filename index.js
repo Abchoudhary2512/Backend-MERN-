0316fe984024d41eda810e2be4d257ec06758dae
ajay2512 <bhakarajay033@gmail.com>
2024-06-03 08:26:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+import http from "http"
+const server = http.createServer((req,res)=>{
+    // console.log("servered");
+    // res.end("<h1>out</h1>")
+    if(req.url === "/about"){
+        res.end("<h1>about page</h1>")
+    }
+    else if(req.url === "/products"){
+        res.end("<h1>products page</h1>")
+    }
+    else if(req.url === "/contact"){
+        res.end("<h1>contact page</h1>")
+    }
+   else if(req.url === "/home"){
+        res.end("<h1>home page</h1>")
+    }
+    else{
+        res.end("page not found 404")
+    }
+  
+})
+server.listen(3000,()=>{
+    console.log("server is working")
+})
